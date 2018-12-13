79282016b78f25251bed31aa1319a83b7867ae17
Fabien Coo <fabien.coo@amigraf.com>
2018-12-13 16:31:49 +0100

    Mise en place des fonctionnalités de blog
    - Itération dans l'objet post
    - Affichage des informations de posts dans un modèle
    - Changement de couleur de fond selon la valeur de LoveIT

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'oc-angular-blog';
+
+  posts = [
+    {
+      title : 'Mon premier article',
+      content : 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+      loveIts : 1,
+    },
+    {
+      title : 'Mon deuxième article',
+      content : 'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+      loveIts : -1,
+    },
+    {
+      title : 'Mon troisième article',
+      content : 'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
+      loveIts : 0,
+    }
+  ]
 }
--- src/app/blog/blog.component.ts
+++ src/app/blog/blog.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-blog',
@@ -7,7 +7,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BlogComponent implements OnInit {
 
+  @Input() postTitle : string;
+  @Input() postContent : string;
+  @Input() postLoveITs: number;
+
   constructor() { }
+
+  addLove(){
+    console.log('Plus d\'amour !')
+  }
+
+  subLove(){
+    console.log('Moins d\'amour !')
+  }
 
   ngOnInit() {
   }
